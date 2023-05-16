819279fdd4a196c5b94b94959da532a603b6ad61
unknown <javidai@code.edu.az>
2023-05-17 03:25:48 +0400

    Practice

---
+++ Api-Solid/Api-Intro/Api-Intro/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api_Intro.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            string[] names = { "Ali","Jale","Pervin" };
+            return Ok(names);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute]int? id)
+        {
+            if (id == null) return BadRequest();
+            
+            return Ok("Ali -" + " " + id);
+        }
+    }
+}
